dcfb2a3e91917a886ff6c723fea178825f2aff9f
Rakshit Gupta <rakshitgupta621@gmail.com>
2021-04-18 10:59:11 +0530

    latest files

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h2>This is about My To-do List</h2><br/>
+            <p> The Todo List is used to add some tasks. You can also describe them by writing the description. You can delete them if you want to by clicking on delete button. It is necessary to write both the title and the description.</p>
+        </div>
+    )
+}
